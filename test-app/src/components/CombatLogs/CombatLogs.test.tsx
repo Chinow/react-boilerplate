fad047cd90c46e147349074b5240e7a1abb8d4f2
Maxime Viry <MaximeV@bittitan.com>
2019-11-02 14:24:58 -0700

    Test

---
+++ test-app/src/components/CombatLogs/CombatLogs.test.tsx
@@ -0,0 +1,22 @@
+import {mount} from 'enzyme';
+import React from 'react';
+import {CombatLogMessage, CombatLogs, LogType} from "./CombatLogs";
+
+describe('<CombatLogs />', () => {
+  it('should render given combat logs', () => {
+    const messages: CombatLogMessage[] = [
+      {
+        message: 'statusMessage',
+        type: LogType.Status
+      },
+      {
+        message: 'damageMessage',
+        type: LogType.Damage
+      }
+    ];
+    const wrapper = mount(<CombatLogs messages={messages}/>)
+
+    expect(wrapper.text()).toContain('statusMessage');
+    expect(wrapper.text()).toContain('damageMessage');
+  })
+});
