a173c4ddc750d76c45c7abab13edf97eac53fd7d
Maxime Viry <MaximeV@bittitan.com>
2019-11-02 10:54:18 -0700

    hover health

--- test-app/src/components/HealthBar/HealthBar.tsx
+++ test-app/src/components/HealthBar/HealthBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './HealthBar.css';
 
 export type HeathBatProp = {
@@ -6,7 +6,24 @@ export type HeathBatProp = {
   maxHealth: number;
 }
 
+type MousePosition = {
+  x: number,
+  y: number
+}
+
 export const HealthBar: React.FC<HeathBatProp> = ({health, maxHealth}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [mousePosition, setMousePosition] = useState<MousePosition>({
+    x: 0,
+    y: 0
+  });
+  const toggleHovered = (value: boolean) => () => setIsHovered(value);
+  const storeMousePosition = (event: React.MouseEvent) => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    });
+  };
   const percent = health / maxHealth * 100;
   let state = 'healthy';
   if (percent < 15) {
@@ -14,9 +31,30 @@ export const HealthBar: React.FC<HeathBatProp> = ({health, maxHealth}) => {
   } else if (percent < 33) {
     state = 'medium';
   }
+
+  const renderHps = () => {
+    const x = mousePosition.x + 10;
+    const y = mousePosition.y + 10;
+    return <div style={{
+      position: 'absolute',
+      left: x,
+      top: y,
+      background: 'white',
+      borderRadius: '4px',
+      border: 'grey 1px solid',
+      padding: '3px'
+    }}>{health}</div>
+  };
+
   return (
-    <div className="health-bar">
+    <div
+      className="health-bar"
+      onMouseMove={storeMousePosition}
+      onMouseEnter={toggleHovered(true)}
+      onMouseLeave={toggleHovered(false)}
+    >
       <div style={{ width: `${percent}%` }} className={`${state} hp-status`} />
+      {isHovered && renderHps()}
     </div>
   )
 };
